6e5fc729dd0e51a1df296909324f751b175f2f24
ale_xxx <vasyekk67@yandex.ru>
2020-11-09 23:46:41 +0300

    parser add func added

--- src/cmd/cmd.h
+++ src/cmd/cmd.h
@@ -6,7 +6,7 @@
 /*   By: jvoor <jvoor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 22:59:37 by kcharla           #+#    #+#             */
-/*   Updated: 2020/11/09 15:41:09 by jvoor            ###   ########.fr       */
+/*   Updated: 2020/11/09 22:49:00 by jvoor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,21 @@
 
 # define MAX_PARAMS		8
 
-typedef struct		s_word
+typedef struct		s_parse_fw
 {
-	char			*w_ptr;
-	int				l;
-}					t_word;
+	t_msg			(*func)(t_rt *, char *);
+	char			*word;
+}					t_parse_fw;
+
 
 /*
 **				cmd_add
 */
 
-int				cmd_add(t_rt *rt, t_word *w);
+# define CMD_ADD_NUM 1
+# define KW_SPHERE "sphere"
+
+int				cmd_parse_add(t_rt *rt, char *str);
 
 /*
 **				cmd_valid
@@ -41,6 +45,18 @@ int				check_word(t_word w, t_word *check, int how_many_args);
 **				cmd_parse
 */
 
+#define CMD_MAIN_NUM 4
+#define KW_ADD "add"
+#define KW_LS "ls"
+#define KW_RM "rm"
+#define KW_SET "set"
+
 t_msg			cmd_parse(t_rt *rt, const char* request);
+
+/*
+**				cmd_parse_tree
+*/
+
+t_msg			cmd_parse_tree(t_rt *rt, char *str, t_parse_fw *arr, int len);
 
 #endif
--- src/srv/srv_parse.c
+++ src/srv/srv_parse.c
@@ -1,12 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   srv_process.c                                      :+:      :+:    :+:   */
+/*   srv_parse.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kcharla <kcharla@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jvoor <jvoor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 16:09:44 by kcharla           #+#    #+#             */
-/*   Updated: 2020/11/02 20:16:07 by kcharla          ###   ########.fr       */
+/*   Updated: 2020/11/09 22:49:41 by jvoor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "rt.h"
+
+t_msg		srv_parse_str(t_srv *srv, const char* request)
+{
+	t_msg	msg;
+	msg.status = MSG_NONE;
+	if (request == NULL)
+		return ((t_msg){MSG_ERROR, "request is NULL pointer"});
+	if (ft_strcmp(request, "exit") == 0)
+	{
+		msg.str = ft_strdup("Closing connection. Have a nice day :) lol\n");
+		msg.status = MSG_EXIT;
+	}
+	else if (ft_strcmp(request, "shutdown") == 0)
+	{
+		msg.str = ft_strdup("Shutting down...\n");
+		msg.status = MSG_SHUT;
+	}
+	else
+	{
+		// if (parse_cmd_line(srv->rt, request) != -1)
+		// 	;//OK
+		msg = cmd_parse_line(srv->rt, request);
+		// msg.status = MSG_OK;
+	}
+	return (msg);
+}
