393ca8fc5a7494a9a7115881bc9cdc52a613961e
ale_xxx <vasyekk67@yandex.ru>
2020-11-13 07:14:16 +0300

    added general obj funcs

---
+++ src/cmd/set/cmd_set_object.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_set_object.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvoor <jvoor@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/13 05:21:03 by jvoor             #+#    #+#             */
+/*   Updated: 2020/11/13 07:13:26 by jvoor            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+t_msg			cmd_set_object_name(t_scn *scn, t_obj *obj, char **source)
+{
+	char		*name;
+	int			res;
+	
+	if (scn == NULL || obj == NULL || source == NULL)
+		return (msg_err("Given pointer is NULL"));
+	if (*source == NULL)
+		return (msg_err("Dereference to NULL pointer"));
+	if ((res = cmd_read_string(source, &name)))
+	{
+		if (res < 0)
+			return (msg_err("Cannot read name string"));
+		return (msg_warn("Cannot read name string"));
+	}
+	if (cmd_read_space_req(source) < 0)
+		return (msg_warn("Syntax error: \' \' expected"));
+	if (scn_component_set_name(scn, obj->id, name))
+		return (msg_err("Cannot set name"));
+	return (msg_oks("Name set"));
+}
+
+t_msg			cmd_set_object_material(t_scn *scn, t_obj *obj, char **source)
+{
+	uint		mat_id;
+	
+	if (scn == NULL || obj == NULL || source == NULL)
+		return (msg_err("Given pointer is NULL"));
+	if (*source == NULL)
+		return (msg_err("Dereference to NULL pointer"));
+	if ((res = cmd_read_id_name(scn, source, &mat_id)))
+	{
+		if (res < 0)
+			return (msg_err("Cannot read material"));
+		return (msg_warn("Cannot read material"));
+	}
+	if (cmd_read_space_req(source) < 0)
+		return (msg_warn("Syntax error: \' \' expected"));
+	if (scn_check_mid(mat_id))
+		return (msg_err("Cannot find material"));
+	obj->material_id = mat_id;
+	return (msg_oks("Material set"));
+}
+
+t_msg			cmd_set_object_param(t_scn *scn, t_obj *obj, char **source)
+{
+    if (scn == NULL || obj == NULL || source == NULL)
+		return (msg_err("Given pointer is NULL"));
+	if (*source == NULL)
+		return (msg_err("Dereference to NULL pointer"));
+	
+    if (ft_str_next_is(*source, KW_PARAM_NAME))
+	{
+		*source += KW_PARAM_LEN;
+		return (cmd_set_object_name(scn, obj, source));
+	}
+	else if (ft_str_next_is(*source, KW_PARAM_MAT))
+	{
+		*source += KW_PARAM_LEN;
+		return (cmd_set_object_mat(scn, obj, source));
+	}
+	return (msg_warn("There is no general object parameters"));
+}
