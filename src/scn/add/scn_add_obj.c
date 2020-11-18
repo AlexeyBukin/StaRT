3df03e12b4626c559c5be3ea09687d50357a5a42
Alex <alexey.green.28@gmail.com>
2020-11-18 08:28:43 +0300

    get by id fix

--- src/scn/add/scn_add_obj.c
+++ src/scn/add/scn_add_obj.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   scn_get_obj_by_id.c                                :+:      :+:    :+:   */
+/*   scn_add_obj.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvoor <jvoor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,33 +12,8 @@
 
 #include "rt.h"
 
-t_obj				*scn_get_obj_by_id_rec(t_obj *grp, t_id id)
-{
-	t_obj			*tmp;
-	size_t			i;
-
-	if (grp == NULL)
-		return (null(rt_err("Given pointer is NULL")));
-	if (grp->id == id)
-		return (grp);
-	if (grp->type == OBJ_GROUP)
-	{
-		i = 0;
-		while (i < grp->content.group.child_num)
-		{
-			tmp = grp->content.group.children[i];
-			tmp = scn_get_obj_by_id_rec(tmp, id);
-			if (tmp != NULL)
-				return (tmp);
-			i++;
-		}
-	}
-	return (NULL);
-}
-
-t_obj				*scn_get_obj_by_id(t_scn *scn, t_id id)
-{
-	if (scn == NULL)
-		return (null(rt_err("Given pointer is NULL")));
-	return (scn_get_obj_by_id_rec(scn->main_group, id));
-}
+//int			scn_add_obj(t_scn *scn, t_obj *obj)
+//{
+//	if (scn == NULL || obj == NULL)
+//		return (rt_err(""))
+//}
