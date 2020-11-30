46f58066db20420b32338775756414fcb5e4ba7a
Irina <ifaley@bk.ru>
2020-11-30 16:18:36 +0300

    leaks fixes

--- src/cmd/set/cmd_set_prepare_obj.c
+++ src/cmd/set/cmd_set_prepare_obj.c
@@ -9,6 +9,7 @@ int 		cmd_set_prepare_obj(t_parser *parser, t_obj *src)
 	if (obj_init(&parser->object, parser->name, src->type))
 		return (rt_err("cmd_set_cone(): malloc error"));
 	ft_memcpy(parser->object, src, sizeof(t_obj));
+	parser->group = parser->object->parent;
 	parser->object->name = ft_strdup(parser->name);
 	parser->transform = &parser->object->transform;
 	parser->container = &parser->object->content.container;
