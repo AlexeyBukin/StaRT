981f2cf767df866af02c73151b37d09f8399159c
Joseth Voor <jvoor@mi-d5.msk.21-school.ru>
2020-12-05 18:48:16 +0300

    tfm qtns apply added

--- src/tfm/tfm_apply.c
+++ src/tfm/tfm_apply.c
@@ -6,21 +6,23 @@
 /*   By: jvoor <jvoor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 23:15:17 by kcharla           #+#    #+#             */
-/*   Updated: 2020/11/13 10:14:39 by jvoor            ###   ########.fr       */
+/*   Updated: 2020/12/05 17:17:57 by jvoor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-int		tfm_apply_from_to(t_tfm *from, t_tfm *to)
+int			tfm_apply_from_to(t_tfm *from, t_tfm *to)
 {
+	t_qtn	q_rotf;
+	t_qtn	q_rott;
+	
 	if (from == NULL || to == NULL)
 		return (rt_err("Given pointer is NULL"));
 	to->pos_global = vec3_plus(from->pos_global, to->pos_local);
-	// TODO : global rotation of rot_origin
-	// TODO : get_quant_rot
-	// TODO : apply_quant_rot
-	//	to->rot_global = mat3x3_identity();
+	q_rotf = matrix_to_qtn(from->rot_global);
+	q_rott = matrix_to_qtn(to->rot_local);
+	to->rot_global = qtn_to_matrix(qtn_mult_qtn(q_rotf, q_rott));
 	to->rot_global = to->rot_local;
 	return (0);
 }
