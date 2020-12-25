b746495ea6b6c9d8f545a0fce43597e7d2c85880
Wiseguy Poudre <wpoudre@mi-r1.msk.21-school.ru>
2020-12-25 04:22:23 +0300

    tfm norm fix

--- src/tfm/tfm_apply.c
+++ src/tfm/tfm_apply.c
@@ -14,15 +14,8 @@
 
 int			tfm_apply_from_to(t_tfm *from, t_tfm *to)
 {
-	// t_qtn	q_rotf;
-	// t_qtn	q_rott;
-	
 	if (from == NULL || to == NULL)
 		return (rt_err("Given pointer is NULL"));
 	to->pos_global = vec3_plus(from->pos_global, to->pos_local);
-	// q_rotf = matrix_to_qtn(from->rot_global);
-	// q_rott = matrix_to_qtn(to->rot_local);
-	// to->rot_global = qtn_to_matrix(qtn_mult_qtn(q_rotf, q_rott));
-	// to->rot_global = to->rot_local;
 	return (0);
 }
